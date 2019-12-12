bd67d1288a4e2d41a90e0c9d17223efdf12ab4d0
wangjy <wangjy@rongzhijia.com>
2019-12-12 17:14:29 +0800

    环问题

--- src/main/java/part5/IScycle.java
+++ src/main/java/part5/IScycle.java
@@ -1,5 +1,7 @@
 package part5;
 
+import com.sun.prism.impl.PrismTrace;
+
 /**
  * @Author: MikeWang
  * @Date: 2019/12/12 11:39 AM
@@ -14,18 +16,31 @@ public class IScycle {
         Node node3 = new Node(7);
         Node node4 = new Node(2);
         Node node5 = new Node(6);
+        Node node6 = new Node(8);
+        Node node7 = new Node(1);
 
         node1.next = node2;
         node2.next = node3;
         node3.next = node4;
         node4.next = node5;
-        node5.next = node2;
+        node5.next = node6;
+        node6.next = node7;
+        node7.next = node4;
 
         System.out.println(isCycle(node1));
+
+        System.out.println(CircleLength(node1));
+        System.out.println(enterCycleNode(node1));
 
 
     }
 
+    /**
+     * 判断是否有环
+     *
+     * @param head
+     * @return
+     */
     public static boolean isCycle(Node head) {
         Node p1 = head;
         Node p2 = head;
@@ -33,12 +48,65 @@ public class IScycle {
             p1 = p1.next;
             p2 = p2.next.next;
             if (p1 == p2) {
+                System.out.println("相遇点：" + p2.data);
                 return true;
             }
         }
         return false;
     }
 
+
+    /**
+     * 判断入环点
+     * @param head
+     * @return 头节点---》入环点 = 相遇点--》遇环点
+     */
+    public static Node enterCycleNode(Node head) {
+        Node p1 = head;
+        Node p2 = head;
+        while (p2 != null && p2.next != null) {
+            p1 = p1.next;
+            p2 = p2.next.next;
+            if (p1 == p2){
+                System.out.println("说明有环");
+                p1 = head;
+                while (p1!=p2){
+                    p1 = p1.next;
+                    p2 = p2.next;
+                }
+                System.out.println("入环点："+p1.data);
+                break;
+            }
+        }
+        return p1;
+    }
+
+    /**
+     * 获取环长
+     *
+     * @param first
+     * @return
+     */
+    public static int CircleLength(Node first) {
+        Node fast = first;
+        Node slow = first;
+        while ((fast != null) && (fast.next != null)) {
+            fast = fast.next.next;
+            slow = slow.next;
+            while (fast == slow) {
+                int len = 1;
+                fast = fast.next.next;
+                slow = slow.next;
+                while (fast != slow) {
+                    len++;
+                    fast = fast.next.next;
+                    slow = slow.next;
+                }
+                return len;
+            }
+        }
+        return 0;
+    }
 
 
     private static class Node {
