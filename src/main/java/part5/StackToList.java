58d49f5c241e899a38f2721cc9f897e451798878
wangjy <wangjy@rongzhijia.com>
2019-12-24 10:32:26 +0800

    用栈实现队列

---
+++ src/main/java/part5/StackToList.java
@@ -0,0 +1,52 @@
+package part5;
+
+import java.util.Stack;
+
+/**
+ * @Author: MikeWang
+ * @Date: 2019/12/24 10:21 AM
+ * @Description:
+ * 用栈实现队列
+ */
+public class StackToList {
+
+    private Stack<Integer> stackA = new Stack<Integer>();
+    private Stack<Integer> stackB = new Stack<Integer>();
+
+    /**
+     * 入队操作
+     * @param element 入队的元素
+     */
+    public void enQueue(int element){
+        stackA.push(element);
+    }
+
+    public Integer deQueue(){
+        if (stackB.isEmpty()){
+            if (stackA.isEmpty()){
+                return null;
+            }
+            transfer();
+        }
+        return stackB.pop();
+    }
+
+    private void transfer(){
+        while (!stackA.isEmpty()){
+            stackB.push(stackA.pop());
+        }
+    }
+
+    public static void main(String[] args) {
+        StackToList stackToList = new StackToList();
+        stackToList.enQueue(1);
+        stackToList.enQueue(2);
+        stackToList.enQueue(3);
+        System.out.println(stackToList.deQueue());
+        System.out.println(stackToList.deQueue());
+        System.out.println(stackToList.deQueue());
+        stackToList.enQueue(4);
+        System.out.println(stackToList.deQueue());
+    }
+
+}
